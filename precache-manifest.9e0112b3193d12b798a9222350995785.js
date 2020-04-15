f554a280c0a70377be595dee90aea7110a4653ce
Arthur Mnyshenko <flushdevelopment@gmail.com>
2020-04-15 14:37:54 +0300

    Updates

---
+++ precache-manifest.9e0112b3193d12b798a9222350995785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3a2e87757510cca249805b00046ba0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55a3e0cea558375a888c",
+    "url": "/static/css/main.0f3e9726.chunk.css"
+  },
+  {
+    "revision": "9001e9c6738b5b50fe5d",
+    "url": "/static/js/2.aef3080c.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.aef3080c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a3e0cea558375a888c",
+    "url": "/static/js/main.7208d14d.chunk.js"
+  },
+  {
+    "revision": "7216c798b30d17ca8650",
+    "url": "/static/js/runtime-main.4201d274.js"
+  }
+]);
