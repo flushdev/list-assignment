df6abe311f336a75d88da3d8925e95a15a7ccc18
Arthur Mnyshenko <flushdevelopment@gmail.com>
2020-04-16 10:55:56 +0300

    add openable incdicator if node has ability for open

--- src/components/ListData/ListData.js
+++ src/components/ListData/ListData.js
@@ -2,19 +2,22 @@ import React from "react";
 
 export default (props) => {
     const render = (element, prevCode) => {
+        console.log(props.data);
         return element.map((el) => {
             return (
                 <>
                     <li
                         onClick={(e) => {
                             prevCode &&
-                                el.children !== null &&
+                                el.children &&
                                 props.changeDisplay(prevCode, el.name);
                             !prevCode && props.changeDisplay(el.name);
                         }}
                         key={el.name + "_li"}
                         name={el.name}
-                        className='li-block'>
+                        className={`li-block${
+                            el.children?.length ? " openable" : ""
+                        }`}>
                         {el.name}
                     </li>
                     {el.children &&
